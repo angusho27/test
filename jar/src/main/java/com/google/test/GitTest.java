680081018769a6830a85b75e64bb204ebc5a9f7b
angusHo <565heruijun@gmail.com>
2023-12-13 14:36:16 +0800

    修改这个test2

---
+++ jar/src/main/java/com/google/test/GitTest.java
@@ -0,0 +1,18 @@
+package com.google.test;
+
+/**
+ * ClassName: GitTest
+ * Package: com.google.test
+ * Description:
+ *
+ * @Author Angus
+ * @Create 2023/12/13 14:29
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("gitTest");
+        System.out.println("testTest");
+        System.out.println("master");
+    }
+}
