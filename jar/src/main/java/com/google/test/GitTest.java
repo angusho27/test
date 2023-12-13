cd5eb1f571a78bcd217f221eaed37ed1c75a565d
angusHo <565heruijun@gmail.com>
2023-12-13 14:43:59 +0800

    添加

---
+++ jar/src/main/java/com/google/test/GitTest.java
@@ -0,0 +1,16 @@
+package com.google.test;
+
+/**
+ * ClassName: GitTest
+ * Package: com.google.test
+ * Description:
+ *
+ * @Author Angus
+ * @Create 2023/12/13 14:43
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("gitTest");
+    }
+}
